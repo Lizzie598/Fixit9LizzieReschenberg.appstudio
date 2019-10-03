6a614f0aec0e2eb85047a27d0a15b3d7c2b7f6a4
Lizzie <ekr74869@creighton.edu>
2019-10-02 19:33:46 -0500

    changes

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,3 @@
+var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+var memberExists = members.includes("Sam")
+console.log(memberExists);
--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,5 +1,14 @@
 
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
+btnSubmitName.onclick=function(){
+  var memberExists = members.include(inptFirstName.value)
+  if (memberExists == "true") 
+    NSB.MsgBox("You are already a member!")
+  else {
+    NSB.MsgBox("You were not a member, but your name is now on our member list.")
+    members.push(inptFirstName.value)
+}
+}
 
 
